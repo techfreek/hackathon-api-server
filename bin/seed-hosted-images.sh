7a3a72de0bbe8fbf35907883bf58a135de3f5789
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-02-20 21:03:52 -0800

    seed-images: added a few more photos to the seeding

--- bin/seed-hosted-images.sh
+++ bin/seed-hosted-images.sh
@@ -13,12 +13,19 @@ if [ ! -d $hosted_images3 ]; then mkdir $hosted_images3; fi
 if [ ! -d ${hosted_images3}_mini ]; then mkdir ${hosted_images3}_mini; fi
 
 server_images_url=hackathon.eecs.wsu.edu/hosted_images/
+images=
+folder=
+grab_images() {
+  for image in $images; do
+    wget -nvc ${server_images_url}$folder/$image -P hosted-images/$folder
+    wget -nvc ${server_images_url}${folder}_mini/$image -P hosted-images/${folder}_mini
+  done
+}
+
 images="IMG_0388.jpg IMG_0390.jpg IMG_0393.jpg IMG_0400.jpg IMG_0402.jpg IMG_0405.jpg"
+folder=hackathon_02
+grab_images
 
-for image in $images; do
-  wget -nvc ${server_images_url}hackathon_02/$image -P $hosted_images2
-  wget -nvc ${server_images_url}hackathon_02_mini/$image -P ${hosted_images2}_mini
-done
-
-wget -nvc ${server_images_url}hackathon_03/sticker.jpg -P $hosted_images3
-wget -nvc ${server_images_url}hackathon_03_mini/sticker.jpg -P ${hosted_images3}_mini
+images="sticker.jpg IMG_6184-2.jpg IMG_6336.jpg IMG_6444.jpg IMG_6517.jpg IMG_7644.jpg IMG_6185.jpg"
+folder=hackathon_03
+grab_images
